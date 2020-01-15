19e6fafc8227e324e1d9c2eae36980a9af76b994
aferns16 <aferns16@estudiantes.unileon.es>
2020-01-15 14:38:37 +0100

    Entradas revisado y acabado

--- cinemaApp/src/views/Entradas.vue
+++ cinemaApp/src/views/Entradas.vue
@@ -1,73 +1,86 @@
 <template>
   <v-app style="background: black">
-    <v-card color="indigo lighten-2" height="150px">
-      <v-toolbar dark height="150px" color="black">
-        <v-toolbar-title id="game">
-          <span style="font-size: 50pt; font-family: 'Helvetica'; ">Asientos</span>
-        </v-toolbar-title>
-       
-        <tr>
-        <v-toolbar-title>
-          <v-combobox
-            :items="filters1"
-            outlined
-            dense
-            rounded
-            label="Entradas"
-            v-on:keyup.enter="onEnterFilter"
-          ></v-combobox>
-        </v-toolbar-title>
-        
-        <v-toolbar-title>
-          <v-combobox
-            :items="filters"
-            outlined
-            dense
-            rounded
-            label="Descuentos"
-            v-on:keyup.enter="onEnterFilter"
-          ></v-combobox>
-        </v-toolbar-title>
-          
-        <v-toolbar-title>
-          <v-text-field
-            dark
-            rounded
-            outlined
-            dense
-            type="text"
-            value ="Precio entrada: 6.00€"
-            readonly
-          ></v-text-field>
-        </v-toolbar-title>
-
-        <v-toolbar-title>
-          <v-text-field
-            dark
-            rounded
-            outlined
-            dense
-            type ="text"
-            value ="Precio total: 4.80€"
-            readonly          
-          ></v-text-field>
-        </v-toolbar-title>     
-        </tr>
-      </v-toolbar>
-    </v-card>
-    
-    <v-divider></v-divider>
-    <v-divider></v-divider>
     <v-content>
-      <v-img
-        align-center
-        justify-center
-        :src="asientosDisponibles[0].src"
-        height="400px"
-        width="600px"
-      >
-        <div style="height: 100%;">{{asiento}}</div>
-      </v-img>
+      <v-container>
+        <v-row>
+          <v-col>
+            <v-toolbar dark height="150px" color="black">
+              <v-toolbar-title style="font-size: 50pt; font-family: 'Helvetica'; ">Asientos</v-toolbar-title>
+            </v-toolbar>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <table cellspacing="0">
+              <tr>
+                <td>
+                  <font color="white">Número de entradas:</font>
+                </td>
+
+                <td>
+                  <v-combobox
+                    :items="filters1"
+                    v-model="entradas"
+                    dark
+                    dense
+                    label="Entradas"
+                    v-on:keyup.enter="onEnterFilter"
+                  ></v-combobox>
+                </td>
+              </tr>
+
+              <tr>
+                <td>
+                  <font color="white">Descuentos:</font>
+                </td>
+
+                <td>
+                  <v-combobox
+                    :items="filters"
+                    v-model="descuentos"
+                    dark
+                    dense
+                    label="Descuentos"
+                    v-on:keyup.enter="onEnterFilter"
+                  ></v-combobox>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <font color="white">Precio de la entrada:</font>
+                </td>
+
+                <td>
+                  <v-text-field v-model="precioEntrada" dark dense type="text" readonly></v-text-field>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <font color="white">Precio total:</font>
+                </td>
+
+                <td>
+                  <v-text-field v-model="precioTotal" dark dense type="text" readonly></v-text-field>
+                </td>
+              </tr>
+            </table>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-content>
+            <v-img
+              align-center
+              justify-center
+              :src="asientosDisponibles[0].src"
+              height="600px"
+              width="800px"
+            >
+              <div style="height: 100%;"></div>
+            </v-img>
+          </v-content>
+        </v-row>
+      </v-container>
     </v-content>
   </v-app>
 </template>
@@ -76,12 +89,14 @@
 
 <script>
 // @ is an alias to /src
-
 export default {
   data() {
     return {
+      entradas: 1,
+      descuentos: "Ninguno",
+      precio: 6,
       filters1: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
-      filters: ["Estudiante", "Jubilado", "Familia numerosa"],
+      filters: ["Ninguno", "Estudiante", "Jubilado", "Familia numerosa"],
       asientosDisponibles: [
         {
           id: 1,
@@ -89,14 +104,26 @@ export default {
             "http://gobiznext.com/wp-content/uploads/2016/10/Mejores-lugares.jpg",
           asiento: "Asientos disponibles"
         }
-      ],
+      ]
     };
   },
-  methods: {
+  computed: {
     precioTotal: function() {
-      2 * 6;
+      var precio = this.entradas * this.precio;
+      if (this.descuentos == "Familia numerosa") precio -= 1.4;
+      if (this.descuentos == "Jubilado") precio -= 1.0;
+      if (this.descuentos == "Estudiante") precio -= 1.2;
+      return precio + " €";
+    },
+    precioEntrada: function() {
+      return this.precio + " €";
     }
-  }
+  },
+  methods: {}
 };
 </script>
-
+<style type="text/css">
+td {
+  padding: 0 200px 0 0;
+}
+</style>
